a94ff365e78a172eace6c854923e6b187c43affa
danilo neves cruz <cruzdanilo@gmail.com>
2018-09-12 21:06:04 -0300

    read files asynchronously

--- index.js
+++ index.js
@@ -2,24 +2,32 @@ const path = require('path');
 const loaderUtils = require('loader-utils');
 const { RawSource, OriginalSource, ConcatSource } = require('webpack-sources');
 
-function getFileList(map, ...dependencies) {
-  return dependencies.reduce((set, dep) => {
-    if (path.extname(dep)) return set.add(dep);
-    return new Set([...set, ...getFileList(map, ...map[dep])]);
-  }, new Set());
+function getFileList(map, dependencies, list = []) {
+  dependencies.forEach((dep) => {
+    if (path.extname(dep)) {
+      if (!list.includes(dep)) list.push(dep);
+    } else getFileList(map, map[dep], list);
+  });
+  return list;
 }
 
-module.exports = function loader(content) {
+module.exports = async function loader(content) {
   const sourcePath = loaderUtils.getRemainingRequest(this);
-  const concat = new ConcatSource(new OriginalSource(content, sourcePath));
+  const concat = new ConcatSource(new OriginalSource(`${content}\n`, sourcePath));
   const options = loaderUtils.getOptions(this) || {};
   if (options.modules) {
+    this.async();
     const config = JSON.parse(this.fs.readFileSync(path.join(this.context, 'moduleConfig.json')));
-    getFileList(config.module, ...options.modules).forEach((f) => {
+    const sources = await Promise.all(getFileList(config.module, options.modules).map(async (f) => {
       const fpath = path.join(this.context, f);
+      const source = await new Promise((resolve, reject) => this.fs.readFile(fpath, (err, data) => {
+        if (err) reject(err);
+        else resolve(new OriginalSource(`${data.toString()}\n`, fpath));
+      }));
       this.addDependency(fpath);
-      concat.add(new OriginalSource(this.fs.readFileSync(fpath).toString(), fpath));
-    });
+      return source;
+    }));
+    sources.forEach(source => concat.add(source));
   }
   concat.add(new RawSource(`export ${options.exports ? `{${options.exports.join()}}` : 'default cc'};\n`));
   const result = concat.sourceAndMap();
